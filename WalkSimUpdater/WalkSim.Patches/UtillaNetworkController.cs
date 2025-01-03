84a2b91f714e4f0ed9c94bc2558583355307d883
Vivid <130241472+Zyverianox@users.noreply.github.com>
2025-01-03 14:27:57 -0500

    Update UtillaNetworkController.cs

--- WalkSimUpdater/WalkSim.Patches/UtillaNetworkController.cs
+++ WalkSimUpdater/WalkSim.Patches/UtillaNetworkController.cs
@@ -7,56 +7,64 @@ namespace WalkSim.Patches;
 
 public class UtillaNetworkController : MonoBehaviourPunCallbacks
 {
-	public static Events events = new Events();
+    public static Events events = new Events();
+    private Events.RoomJoinedArgs lastRoom;
 
-	private Events.RoomJoinedArgs lastRoom;
+    public override void OnJoinedRoom()
+    {
+        bool isPrivate = false;
+        string gamemode = "";
+        if (PhotonNetwork.CurrentRoom != null)
+        {
+            Room currentRoom = PhotonNetwork.NetworkingClient.CurrentRoom;
+            isPrivate = !currentRoom.IsVisible || ((Dictionary<object, object>)currentRoom.CustomProperties).ContainsKey("Description");
+            if (((Dictionary<object, object>)currentRoom.CustomProperties).TryGetValue("gameMode", out object value))
+            {
+                gamemode = value as string;
+            }
+        }
 
-	public override void OnJoinedRoom()
-	{
-		bool isPrivate = false;
-		string text = "";
-		if (PhotonNetwork.CurrentRoom != null)
-		{
-			Room currentRoom = PhotonNetwork.NetworkingClient.CurrentRoom;
-			isPrivate = !currentRoom.IsVisible || ((Dictionary<object, object>)(object)((RoomInfo)currentRoom).CustomProperties).ContainsKey((object)"Description");
-			if (((Dictionary<object, object>)(object)((RoomInfo)currentRoom).CustomProperties).TryGetValue((object)"gameMode", out object value))
-			{
-				text = value as string;
-			}
-		}
-		string text2 = "";
-		Dictionary<string, string> dictionary = new Dictionary<string, string>
-		{
-			{ "INFECTION", "INFECTION" },
-			{ "CASUAL", "CASUAL" },
-			{ "HUNT", "HUNT" },
-			{ "BATTLE", "PAINTBRAWL" }
-		};
-		foreach (KeyValuePair<string, string> item in dictionary)
-		{
-			if (text.Contains(item.Key))
-			{
-				text2 = item.Value;
-				break;
-			}
-		}
-		((GorillaComputer)GorillaComputer.instance).currentGameModeText.Value = "CURRENT MODE\n" + text2;
-		Events.RoomJoinedArgs e = new Events.RoomJoinedArgs
-		{
-			isPrivate = isPrivate,
-			Gamemode = text
-		};
-		events.TriggerRoomJoin(e);
-		lastRoom = e;
-	}
+        string displayMode = GetDisplayMode(gamemode);
 
-	public override void OnLeftRoom()
-	{
-		if (lastRoom != null)
-		{
-			events.TriggerRoomLeft(lastRoom);
-			lastRoom = null;
-		}
-		((GorillaComputer)GorillaComputer.instance).currentGameModeText.Value = "CURRENT MODE\n-NOT IN ROOM-";
-	}
+        GorillaComputer.instance.currentGameModeText.Value = "CURRENT MODE\n" + displayMode;
+        Events.RoomJoinedArgs e = new Events.RoomJoinedArgs
+        {
+            isPrivate = isPrivate,
+            Gamemode = gamemode
+        };
+        events.TriggerRoomJoin(e);
+        lastRoom = e;
+    }
+
+    public override void OnLeftRoom()
+    {
+        if (lastRoom != null)
+        {
+            events.TriggerRoomLeft(lastRoom);
+            lastRoom = null;
+        }
+        GorillaComputer.instance.currentGameModeText.Value = "CURRENT MODE\n-NOT IN ROOM-";
+    }
+
+    private string GetDisplayMode(string gamemode)
+    {
+        Dictionary<string, string> gamemodeMapping = new Dictionary<string, string>
+        {
+            { "INFECTION", "INFECTION" },
+            { "CASUAL", "CASUAL" },
+            { "HUNT", "HUNT" },
+            { "BATTLE", "PAINTBRAWL" },
+            { "NEW_MODE1", "NEW_MODE1_DISPLAY" }, // Add new game modes here
+            { "NEW_MODE2", "NEW_MODE2_DISPLAY" }  // Add new game modes here
+        };
+
+        foreach (KeyValuePair<string, string> item in gamemodeMapping)
+        {
+            if (gamemode.Contains(item.Key))
+            {
+                return item.Value;
+            }
+        }
+        return "UNKNOWN";
+    }
 }
