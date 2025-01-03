02a272b1a1db4954ce858bffb6aba0ec1dca34cd
Vivid <130241472+Zyverianox@users.noreply.github.com>
2025-01-03 14:15:16 -0500

    Update InputHandler.cs

--- WalkSimUpdater/WalkSim.Plugin/InputHandler.cs
+++ WalkSimUpdater/WalkSim.Plugin/InputHandler.cs
@@ -14,202 +14,157 @@ namespace WalkSim.Plugin;
 
 public class InputHandler : MonoBehaviour
 {
-	public static InputHandler Instance;
+    public static InputHandler Instance;
 
-	public static Vector3 inputDirection;
+    public static Vector3 inputDirection;
 
-	public static Vector3 inputDirectionNoY;
+    public static Vector3 inputDirectionNoY;
 
-	public static string deviceName = "";
+    public static string deviceName = "";
 
-	public static string devicePrefix = "";
+    public static string devicePrefix = "";
 
-	private bool Jump => ((ButtonControl)Keyboard.current.spaceKey).wasPressedThisFrame;
+    private void Awake()
+    {
+        Instance = this;
+        ValidateDevices();
+    }
 
-	private void Awake()
-	{
-		Instance = this;
-		ValidateDevices();
-	}
+    private void Update()
+    {
+        try
+        {
+            if (ComputerGUI.Instance != null && Plugin.Instance.Enabled && !ComputerGUI.Instance.IsInUse)
+            {
+                GetInputDirection();
+                if (Keyboard.current.escapeKey.wasPressedThisFrame)
+                {
+                    HeadDriver.Instance.LockCursor = !HeadDriver.Instance.LockCursor;
+                }
+                if (Keyboard.current.cKey.wasPressedThisFrame)
+                {
+                    HeadDriver.Instance.ToggleCam();
+                }
+                RadialMenu radialMenu = Plugin.Instance.radialMenu;
+                radialMenu.enabled = Keyboard.current.tabKey.isPressed;
+                radialMenu.gameObject.SetActive(Keyboard.current.tabKey.isPressed);
+                if (Keyboard.current.digit1Key.wasPressedThisFrame)
+                {
+                    EnableEmote(EmoteAnimator.Emote.Wave);
+                }
+                if (Keyboard.current.digit2Key.wasPressedThisFrame)
+                {
+                    EnableEmote(EmoteAnimator.Emote.Point);
+                }
+                if (Keyboard.current.digit3Key.wasPressedThisFrame)
+                {
+                    EnableEmote(EmoteAnimator.Emote.ThumbsUp);
+                }
+                if (Keyboard.current.digit4Key.wasPressedThisFrame)
+                {
+                    EnableEmote(EmoteAnimator.Emote.ThumbsDown);
+                }
+                if (Keyboard.current.digit5Key.wasPressedThisFrame)
+                {
+                    EnableEmote(EmoteAnimator.Emote.Shrug);
+                }
+                if (Keyboard.current.digit6Key.wasPressedThisFrame)
+                {
+                    EnableEmote(EmoteAnimator.Emote.Dance);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.LogError($"An error occurred in InputHandler.Update: {ex}");
+        }
+    }
 
-	private void Update()
-	{
-		try
-		{
-			if ((Object)(object)ComputerGUI.Instance != (Object)null && Plugin.Instance.Enabled && !ComputerGUI.Instance.IsInUse)
-			{
-				GetInputDirection();
-				if (((ButtonControl)Keyboard.current.escapeKey).wasPressedThisFrame)
-				{
-					HeadDriver.Instance.LockCursor = !HeadDriver.Instance.LockCursor;
-				}
-				if (((ButtonControl)Keyboard.current.cKey).wasPressedThisFrame)
-				{
-					HeadDriver.Instance.ToggleCam();
-				}
-				RadialMenu radialMenu = Plugin.Instance.radialMenu;
-				((Behaviour)radialMenu).enabled = ((ButtonControl)Keyboard.current.tabKey).isPressed;
-				((Component)radialMenu).gameObject.SetActive(((ButtonControl)Keyboard.current.tabKey).isPressed);
-				if (((ButtonControl)Keyboard.current.digit1Key).wasPressedThisFrame)
-				{
-					EnableEmote(EmoteAnimator.Emote.Wave);
-				}
-				if (((ButtonControl)Keyboard.current.digit2Key).wasPressedThisFrame)
-				{
-					EnableEmote(EmoteAnimator.Emote.Point);
-				}
-				if (((ButtonControl)Keyboard.current.digit3Key).wasPressedThisFrame)
-				{
-					EnableEmote(EmoteAnimator.Emote.ThumbsUp);
-				}
-				if (((ButtonControl)Keyboard.current.digit4Key).wasPressedThisFrame)
-				{
-					EnableEmote(EmoteAnimator.Emote.ThumbsDown);
-				}
-				if (((ButtonControl)Keyboard.current.digit5Key).wasPressedThisFrame)
-				{
-					EnableEmote(EmoteAnimator.Emote.Shrug);
-				}
-				if (((ButtonControl)Keyboard.current.digit6Key).wasPressedThisFrame)
-				{
-					EnableEmote(EmoteAnimator.Emote.Dance);
-				}
-			}
-		}
-		catch
-		{
-		}
-	}
+    private void EnableEmote(EmoteAnimator.Emote emote)
+    {
+        EmoteAnimator emoteAnimator = Plugin.Instance.emoteAnimator as EmoteAnimator;
+        Rig.Instance.Animator = emoteAnimator;
+        emoteAnimator.emote = emote;
+    }
 
-	private void EnableEmote(EmoteAnimator.Emote emote)
-	{
-		EmoteAnimator emoteAnimator = Plugin.Instance.emoteAnimator as EmoteAnimator;
-		Rig.Instance.Animator = emoteAnimator;
-		emoteAnimator.emote = emote;
-	}
+    private void GetInputDirection()
+    {
+        Vector3 val = KeyboardInput();
+        if (val.magnitude > 0f)
+        {
+            inputDirection = val.normalized;
+            inputDirectionNoY = val;
+            inputDirectionNoY.y = 0f;
+            inputDirectionNoY.Normalize();
+            return;
+        }
+        InputDevice leftControllerDevice = ControllerInputPoller.instance.leftControllerDevice;
+        InputDevice rightControllerDevice = ControllerInputPoller.instance.rightControllerDevice;
+        leftControllerDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 val2);
+        rightControllerDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 val3);
+        float x = val2.x;
+        float y = val3.y;
+        float y2 = val2.y;
+        Vector3 val4 = new Vector3(x, y, y2);
+        inputDirection = val4.normalized;
+        inputDirectionNoY = new Vector3(x, 0f, y2);
+        inputDirectionNoY.Normalize();
+    }
 
-	private void GetInputDirection()
-	{
-		//IL_0002: Unknown result type (might be due to invalid IL or missing references)
-		//IL_0007: Unknown result type (might be due to invalid IL or missing references)
-		//IL_0055: Unknown result type (might be due to invalid IL or missing references)
-		//IL_005a: Unknown result type (might be due to invalid IL or missing references)
-		//IL_0063: Unknown result type (might be due to invalid IL or missing references)
-		//IL_0068: Unknown result type (might be due to invalid IL or missing references)
-		//IL_006c: Unknown result type (might be due to invalid IL or missing references)
-		//IL_007b: Unknown result type (might be due to invalid IL or missing references)
-		//IL_0088: Unknown result type (might be due to invalid IL or missing references)
-		//IL_0090: Unknown result type (might be due to invalid IL or missing references)
-		//IL_0098: Unknown result type (might be due to invalid IL or missing references)
-		//IL_00a3: Unknown result type (might be due to invalid IL or missing references)
-		//IL_00a8: Unknown result type (might be due to invalid IL or missing references)
-		//IL_00ac: Unknown result type (might be due to invalid IL or missing references)
-		//IL_00b1: Unknown result type (might be due to invalid IL or missing references)
-		//IL_00bd: Unknown result type (might be due to invalid IL or missing references)
-		//IL_00c2: Unknown result type (might be due to invalid IL or missing references)
-		//IL_001f: Unknown result type (might be due to invalid IL or missing references)
-		//IL_0024: Unknown result type (might be due to invalid IL or missing references)
-		//IL_0029: Unknown result type (might be due to invalid IL or missing references)
-		//IL_002a: Unknown result type (might be due to invalid IL or missing references)
-		Vector3 val = KeyboardInput();
-		if (((Vector3)(ref val)).magnitude > 0f)
-		{
-			inputDirection = ((Vector3)(ref val)).normalized;
-			inputDirectionNoY = val;
-			inputDirectionNoY.y = 0f;
-			((Vector3)(ref inputDirectionNoY)).Normalize();
-			return;
-		}
-		InputDevice leftControllerDevice = ((ControllerInputPoller)ControllerInputPoller.instance).leftControllerDevice;
-		InputDevice rightControllerDevice = ((ControllerInputPoller)ControllerInputPoller.instance).rightControllerDevice;
-		Vector2 val2 = default(Vector2);
-		((InputDevice)(ref leftControllerDevice)).TryGetFeatureValue(CommonUsages.primary2DAxis, ref val2);
-		Vector2 val3 = default(Vector2);
-		((InputDevice)(ref rightControllerDevice)).TryGetFeatureValue(CommonUsages.primary2DAxis, ref val3);
-		float x = val2.x;
-		float y = val3.y;
-		float y2 = val2.y;
-		Vector3 val4 = new Vector3(x, y, y2);
-		inputDirection = ((Vector3)(ref val4)).normalized;
-		inputDirectionNoY = new Vector3(x, 0f, y2);
-		((Vector3)(ref inputDirectionNoY)).Normalize();
-	}
+    private Vector3 KeyboardInput()
+    {
+        float num = 0f;
+        float num2 = 0f;
+        float num3 = 0f;
+        if (Keyboard.current.aKey.isPressed)
+        {
+            num -= 1f;
+        }
+        if (Keyboard.current.dKey.isPressed)
+        {
+            num += 1f;
+        }
+        if (Keyboard.current.sKey.isPressed)
+        {
+            num2 -= 1f;
+        }
+        if (Keyboard.current.wKey.isPressed)
+        {
+            num2 += 1f;
+        }
+        if (Keyboard.current.ctrlKey.isPressed)
+        {
+            num3 -= 1f;
+        }
+        if (Keyboard.current.spaceKey.isPressed)
+        {
+            num3 += 1f;
+        }
+        return new Vector3(num, num3, num2);
+    }
 
-	private Vector3 KeyboardInput()
-	{
-		//IL_00ce: Unknown result type (might be due to invalid IL or missing references)
-		//IL_00d3: Unknown result type (might be due to invalid IL or missing references)
-		//IL_00d7: Unknown result type (might be due to invalid IL or missing references)
-		float num = 0f;
-		float num2 = 0f;
-		float num3 = 0f;
-		if (((ButtonControl)Keyboard.current.aKey).isPressed)
-		{
-			num -= 1f;
-		}
-		if (((ButtonControl)Keyboard.current.dKey).isPressed)
-		{
-			num += 1f;
-		}
-		if (((ButtonControl)Keyboard.current.sKey).isPressed)
-		{
-			num2 -= 1f;
-		}
-		if (((ButtonControl)Keyboard.current.wKey).isPressed)
-		{
-			num2 += 1f;
-		}
-		if (Keyboard.current.ctrlKey.isPressed)
-		{
-			num3 -= 1f;
-		}
-		if (((ButtonControl)Keyboard.current.spaceKey).isPressed)
-		{
-			num3 += 1f;
-		}
-		return new Vector3(num, num3, num2);
-	}
-
-	private void ValidateDevices()
-	{
-		Events.RoomJoined += delegate(object _, Events.RoomJoinedArgs args)
-		{
-			//IL_0008: Unknown result type (might be due to invalid IL or missing references)
-			//IL_000d: Unknown result type (might be due to invalid IL or missing references)
-			//IL_0010: Unknown result type (might be due to invalid IL or missing references)
-			//IL_0015: Unknown result type (might be due to invalid IL or missing references)
-			//IL_009c: Unknown result type (might be due to invalid IL or missing references)
-			//IL_00a1: Unknown result type (might be due to invalid IL or missing references)
-			//IL_00c3: Unknown result type (might be due to invalid IL or missing references)
-			//IL_00c8: Unknown result type (might be due to invalid IL or missing references)
-			List<string> list = new List<string>();
-			Enumerator<InputDevice> enumerator = InputSystem.devices.GetEnumerator();
-			try
-			{
-				while (enumerator.MoveNext())
-				{
-					InputDevice current = enumerator.Current;
-					list.Add(((InputControl)current).name);
-				}
-			}
-			finally
-			{
-				((IDisposable)enumerator).Dispose();
-			}
-			string gamemode = args.Gamemode;
-			Logging.Debug(args.Gamemode);
-			if (!gamemode.Contains(devicePrefix + deviceName) && ((Behaviour)Rig.Instance).enabled)
-			{
-				while (Application.isFocused)
-				{
-					GorillaControllerType controllerType = ((ControllerInputPoller)ControllerInputPoller.instance).controllerType;
-					if (((object)(GorillaControllerType)(ref controllerType)).Equals((object?)deviceName))
-					{
-						controllerType = ((ControllerInputPoller)ControllerInputPoller.instance).controllerType;
-						deviceName = ((object)(GorillaControllerType)(ref controllerType)).ToString();
-					}
-				}
-			}
-		};
-	}
+    private void ValidateDevices()
+    {
+        Events.RoomJoined += delegate(object _, Events.RoomJoinedArgs args)
+        {
+            List<string> list = new List<string>();
+            foreach (var device in InputSystem.devices)
+            {
+                list.Add(device.name);
+            }
+            string gamemode = args.Gamemode;
+            Logging.Debug(args.Gamemode);
+            if (!gamemode.Contains(devicePrefix + deviceName) && Rig.Instance.enabled)
+            {
+                while (Application.isFocused)
+                {
+                    GorillaControllerType controllerType = ControllerInputPoller.instance.controllerType;
+                    if (controllerType.Equals(deviceName))
+                    {
+                        deviceName = controllerType.ToString();
+                    }
+                }
+            }
+        };
+    }
 }
