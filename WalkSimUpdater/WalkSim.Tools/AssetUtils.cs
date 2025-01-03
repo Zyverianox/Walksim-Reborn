2e5b5ebca0de70f9e434bff6c7fe793a29d01107
Vivid <130241472+Zyverianox@users.noreply.github.com>
2025-01-03 14:19:12 -0500

    Update AssetUtils.cs

--- WalkSimUpdater/WalkSim.Tools/AssetUtils.cs
+++ WalkSimUpdater/WalkSim.Tools/AssetUtils.cs
@@ -7,29 +7,29 @@ namespace WalkSim.Tools;
 
 public static class AssetUtils
 {
-	private static string FormatPath(string path)
-	{
-		return path.Replace("/", ".").Replace("\\", ".");
-	}
+    private static string FormatPath(string path)
+    {
+        return path.Replace("/", ".").Replace("\\", ".");
+    }
 
-	public static AssetBundle LoadAssetBundle(string path)
-	{
-		path = FormatPath(path);
-		Stream manifestResourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(path);
-		AssetBundle result = AssetBundle.LoadFromStream(manifestResourceStream);
-		manifestResourceStream.Close();
-		return result;
-	}
+    public static AssetBundle LoadAssetBundle(string path)
+    {
+        path = FormatPath(path);
+        using (Stream manifestResourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(path))
+        {
+            return AssetBundle.LoadFromStream(manifestResourceStream);
+        }
+    }
 
-	public static string[] GetResourceNames()
-	{
-		Assembly callingAssembly = Assembly.GetCallingAssembly();
-		string[] manifestResourceNames = callingAssembly.GetManifestResourceNames();
-		if (manifestResourceNames == null)
-		{
-			Console.WriteLine("No manifest resources found.");
-			return null;
-		}
-		return manifestResourceNames;
-	}
+    public static string[] GetResourceNames()
+    {
+        Assembly callingAssembly = Assembly.GetCallingAssembly();
+        string[] manifestResourceNames = callingAssembly.GetManifestResourceNames();
+        if (manifestResourceNames == null || manifestResourceNames.Length == 0)
+        {
+            Debug.LogWarning("No manifest resources found.");
+            return Array.Empty<string>();
+        }
+        return manifestResourceNames;
+    }
 }
