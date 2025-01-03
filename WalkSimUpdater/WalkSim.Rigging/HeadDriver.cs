047e6edb9d05ebd02de99a1e6bdc29c71270fa54
Vivid <130241472+Zyverianox@users.noreply.github.com>
2025-01-03 14:24:14 -0500

    Update HeadDriver.cs

--- WalkSimUpdater/WalkSim.Rigging/HeadDriver.cs
+++ WalkSimUpdater/WalkSim.Rigging/HeadDriver.cs
@@ -8,137 +8,105 @@ namespace WalkSim.Rigging;
 
 public class HeadDriver : MonoBehaviour
 {
-	public static HeadDriver Instance;
+    public static HeadDriver Instance;
 
-	public Transform thirpyTarget;
+    public Transform thirpyTarget;
+    public Transform head;
+    public GameObject cameraObject;
 
-	public Transform head;
+    private Vector3 offset = new Vector3(0f, 0f, 0f);
+    private Camera overrideCam;
 
-	public GameObject cameraObject;
+    public bool turn = true;
 
-	private Vector3 offset = new Vector3(0f, 0f, 0f);
+    private bool _lockCursor;
 
-	private Camera overrideCam;
+    public bool LockCursor
+    {
+        get => _lockCursor;
+        set
+        {
+            _lockCursor = value;
+            Cursor.lockState = _lockCursor ? CursorLockMode.Locked : CursorLockMode.None;
+            Cursor.visible = !_lockCursor;
+        }
+    }
 
-	public bool turn = true;
+    public bool FirstPerson
+    {
+        get => overrideCam.enabled;
+        set => overrideCam.enabled = value;
+    }
 
-	private bool _lockCursor;
+    private void Awake()
+    {
+        Instance = this;
+    }
 
-	public bool LockCursor
-	{
-		get
-		{
-			return _lockCursor;
-		}
-		set
-		{
-			_lockCursor = value;
-			if (_lockCursor)
-			{
-				Cursor.lockState = (CursorLockMode)1;
-				Cursor.visible = false;
-			}
-			else
-			{
-				Cursor.lockState = (CursorLockMode)0;
-				Cursor.visible = true;
-			}
-		}
-	}
+    private void Start()
+    {
+        var thirdPersonFollow = FindObjectOfType<Cinemachine3rdPersonFollow>();
+        thirpyTarget = thirdPersonFollow.VirtualCamera.Follow;
+        var componentInParent = thirdPersonFollow.GetComponentInParent<Camera>();
+        cameraObject = Instantiate(WalkSim.Plugin.Plugin.Instance.bundle.LoadAsset<GameObject>("Override Camera"));
+        overrideCam = cameraObject.GetComponent<Camera>();
+        overrideCam.nearClipPlane = componentInParent.nearClipPlane;
+        overrideCam.farClipPlane = componentInParent.farClipPlane;
+        overrideCam.cullingMask = componentInParent.cullingMask;
+        overrideCam.depth = componentInParent.depth + 1f;
+        overrideCam.targetDisplay = componentInParent.targetDisplay;
+        overrideCam.fieldOfView = 90f;
+        overrideCam.enabled = false;
+    }
 
-	public bool FirstPerson
-	{
-		get
-		{
-			return ((Behaviour)overrideCam).enabled;
-		}
-		set
-		{
-			((Behaviour)overrideCam).enabled = value;
-		}
-	}
+    private void LateUpdate()
+    {
+        cameraObject.transform.position = Player.Instance.headCollider.transform.TransformPoint(offset);
+        cameraObject.transform.forward = head.forward;
+        if (turn)
+        {
+            var mouseDelta = Mouse.current.delta.ReadValue();
+            Player.Instance.Turn(mouseDelta.x / 10f);
+            var eulerAngles = GorillaTagger.Instance.offlineVRRig.headConstraint.eulerAngles;
+            eulerAngles.x -= mouseDelta.y / 10f;
+            if (eulerAngles.x > 180f)
+            {
+                eulerAngles.x -= 360f;
+            }
+            eulerAngles.x = Mathf.Clamp(eulerAngles.x, -60f, 60f);
+            GorillaTagger.Instance.offlineVRRig.headConstraint.eulerAngles = eulerAngles;
+            eulerAngles.y += 90f;
+            thirpyTarget.localEulerAngles = new Vector3(eulerAngles.x, 0f, 0f);
+            Player.Instance.headCollider.transform.localEulerAngles = new Vector3(eulerAngles.x, 0f, 0f);
+        }
+    }
 
-	private void Awake()
-	{
-		Instance = this;
-	}
+    private void OverrideHeadMovement()
+    {
+        head = GorillaTagger.Instance.offlineVRRig.head.rigTarget;
+    }
 
-	private void Start()
-	{
-		Cinemachine3rdPersonFollow val = Object.FindObjectOfType<Cinemachine3rdPersonFollow>();
-		thirpyTarget = ((CinemachineComponentBase)val).VirtualCamera.Follow;
-		Camera componentInParent = ((Component)val).gameObject.GetComponentInParent<Camera>();
-		cameraObject = WalkSim.Plugin.Plugin.Instance.bundle.LoadAsset<GameObject>("Override Camera");
-		cameraObject = Object.Instantiate<GameObject>(cameraObject);
-		overrideCam = cameraObject.GetComponent<Camera>();
-		overrideCam.nearClipPlane = componentInParent.nearClipPlane;
-		overrideCam.farClipPlane = componentInParent.farClipPlane;
-		overrideCam.cullingMask = componentInParent.cullingMask;
-		overrideCam.depth = componentInParent.depth + 1f;
-		overrideCam.targetDisplay = componentInParent.targetDisplay;
-		overrideCam.fieldOfView = 90f;
-		((Behaviour)overrideCam).enabled = false;
-	}
+    internal void ToggleCam()
+    {
+        overrideCam.enabled = !overrideCam.enabled;
+    }
 
-	private void LateUpdate()
-	{
-		//IL_001c: Unknown result type (might be due to invalid IL or missing references)
-		//IL_0021: Unknown result type (might be due to invalid IL or missing references)
-		//IL_003d: Unknown result type (might be due to invalid IL or missing references)
-		//IL_008c: Unknown result type (might be due to invalid IL or missing references)
-		//IL_0091: Unknown result type (might be due to invalid IL or missing references)
-		//IL_00b7: Unknown result type (might be due to invalid IL or missing references)
-		//IL_00e0: Unknown result type (might be due to invalid IL or missing references)
-		//IL_0109: Unknown result type (might be due to invalid IL or missing references)
-		//IL_0126: Unknown result type (might be due to invalid IL or missing references)
-		//IL_0136: Unknown result type (might be due to invalid IL or missing references)
-		//IL_0150: Unknown result type (might be due to invalid IL or missing references)
-		//IL_0160: Unknown result type (might be due to invalid IL or missing references)
-		cameraObject.transform.position = ((Component)Player.Instance.headCollider).transform.TransformPoint(offset);
-		cameraObject.transform.forward = head.forward;
-		if (turn)
-		{
-			Player.Instance.Turn(((Vector2)((InputControl<Vector2>)(object)((Pointer)Mouse.current).delta).value).x / 10f);
-			Vector3 eulerAngles = GorillaTagger.Instance.offlineVRRig.headConstraint.eulerAngles;
-			eulerAngles.x -= ((Vector2)((InputControl<Vector2>)(object)((Pointer)Mouse.current).delta).value).y / 10f;
-			if (eulerAngles.x > 180f)
-			{
-				eulerAngles.x -= 360f;
-			}
-			eulerAngles.x = Mathf.Clamp(eulerAngles.x, -60f, 60f);
-			GorillaTagger.Instance.offlineVRRig.headConstraint.eulerAngles = eulerAngles;
-			eulerAngles.y += 90f;
-			thirpyTarget.localEulerAngles = new Vector3(eulerAngles.x, 0f, 0f);
-			((Component)Player.Instance.headCollider).transform.localEulerAngles = new Vector3(eulerAngles.x, 0f, 0f);
-		}
-	}
+    private void OnEnable()
+    {
+        Logging.Debug("Enabled");
+        if (Rig.Instance.Animator != null)
+        {
+            LockCursor = true;
+            OverrideHeadMovement();
+        }
+    }
 
-	private void OverrideHeadMovement()
-	{
-		head = GorillaTagger.Instance.offlineVRRig.head.rigTarget;
-	}
-
-	internal void ToggleCam()
-	{
-		((Behaviour)overrideCam).enabled = !((Behaviour)overrideCam).enabled;
-	}
-
-	private void OnEnable()
-	{
-		Logging.Debug("Enabled");
-		if (!((Object)(object)Rig.Instance.Animator == (Object)null))
-		{
-			LockCursor = true;
-			OverrideHeadMovement();
-		}
-	}
-
-	private void OnDisable()
-	{
-		if (Object.op_Implicit((Object)(object)head))
-		{
-			LockCursor = false;
-			GorillaTagger.Instance.offlineVRRig.head.rigTarget = head;
-		}
-	}
+    private void OnDisable()
+    {
+        if (head != null)
+        {
+            LockCursor = false;
+            GorillaTagger.Instance.offlineVRRig.head.rigTarget = head;
+        }
+    }
 }
