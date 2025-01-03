44935f8802e0868d5eb9151a1e07f62b3ab326b5
Vivid <130241472+Zyverianox@users.noreply.github.com>
2025-01-03 14:12:48 -0500

    Update PluginInfo.cs

--- WalkSimUpdater/WalkSim.Plugin/PluginInfo.cs
+++ WalkSimUpdater/WalkSim.Plugin/PluginInfo.cs
@@ -2,9 +2,7 @@ namespace WalkSim.Plugin;
 
 internal class PluginInfo
 {
-	public const string GUID = "com.kylethescientist.gorillatag.walksimulator";
-
-	public const string Name = "WalkSimulator";
-
-	public const string Version = "2.0.1";
+    public const string GUID = "com.kylethescientist.gorillatag.walksimulator";
+    public const string Name = "WalkSimulator";
+    public const string Version = "2.0.1";
 }
