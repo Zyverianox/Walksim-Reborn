037859d17054ac7e4b80dd4226a304fb576db319
Vivid <130241472+Zyverianox@users.noreply.github.com>
2025-01-03 14:32:03 -0500

    Update EventHandlerExtensions.cs

--- WalkSimUpdater/WalkSim.Patches/EventHandlerExtensions.cs
+++ WalkSimUpdater/WalkSim.Patches/EventHandlerExtensions.cs
@@ -1,41 +1,42 @@
 using System;
 using UnityEngine;
 
-namespace WalkSim.Patches;
-
-public static class EventHandlerExtensions
+namespace WalkSim.Patches
 {
-	public static void SafeInvoke(this EventHandler handler, object sender, EventArgs e)
-	{
-		Delegate[] array = handler?.GetInvocationList();
-		for (int i = 0; i < array.Length; i++)
-		{
-			EventHandler eventHandler = (EventHandler)array[i];
-			try
-			{
-				eventHandler?.Invoke(sender, e);
-			}
-			catch (Exception ex)
-			{
-				Debug.LogError((object)ex);
-			}
-		}
-	}
+    public static class EventHandlerExtensions
+    {
+        public static void SafeInvoke(this EventHandler handler, object sender, EventArgs e)
+        {
+            if (handler == null) return;
 
-	public static void SafeInvoke<T>(this EventHandler<T> handler, object sender, T e) where T : EventArgs
-	{
-		Delegate[] array = handler?.GetInvocationList();
-		for (int i = 0; i < array.Length; i++)
-		{
-			EventHandler<T> eventHandler = (EventHandler<T>)array[i];
-			try
-			{
-				eventHandler?.Invoke(sender, e);
-			}
-			catch (Exception ex)
-			{
-				Debug.LogError((object)ex);
-			}
-		}
-	}
+            foreach (Delegate del in handler.GetInvocationList())
+            {
+                try
+                {
+                    ((EventHandler)del).Invoke(sender, e);
+                }
+                catch (Exception ex)
+                {
+                    Debug.LogError($"Exception in event handler: {del.Method.Name}, Exception: {ex}");
+                }
+            }
+        }
+
+        public static void SafeInvoke<T>(this EventHandler<T> handler, object sender, T e) where T : EventArgs
+        {
+            if (handler == null) return;
+
+            foreach (Delegate del in handler.GetInvocationList())
+            {
+                try
+                {
+                    ((EventHandler<T>)del).Invoke(sender, e);
+                }
+                catch (Exception ex)
+                {
+                    Debug.LogError($"Exception in event handler: {del.Method.Name}, Exception: {ex}");
+                }
+            }
+        }
+    }
 }
