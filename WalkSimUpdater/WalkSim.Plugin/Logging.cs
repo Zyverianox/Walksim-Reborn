0e8f417a9b52853b5c781d4d759698056aecf6ef
Vivid <130241472+Zyverianox@users.noreply.github.com>
2025-01-03 14:09:05 -0500

    Improved Logging.cs.
    The logging method now includes the log level to make it more flexible and reusable.
    
    Improved exception logging to include both message and stack trace.

--- WalkSimUpdater/WalkSim.Plugin/Logging.cs
+++ WalkSimUpdater/WalkSim.Plugin/Logging.cs
@@ -7,58 +7,59 @@ namespace WalkSim.Plugin;
 
 public static class Logging
 {
-	private static ManualLogSource logger;
+    private static ManualLogSource logger;
 
-	public static int DebuggerLines = 20;
+    public static int DebuggerLines = 20;
 
-	public static void Init()
-	{
-		logger = Logger.CreateLogSource("WalkSimulator");
-	}
+    public static void Init()
+    {
+        logger = Logger.CreateLogSource("WalkSimulator");
+    }
 
-	public static void Exception(Exception e)
-	{
-		MethodBase method = new StackTrace().GetFrame(1).GetMethod();
-		logger.LogWarning((object)("(" + method.ReflectedType.Name + "." + method.Name + "()) " + string.Join(" ", e.Message, e.StackTrace)));
-	}
+    public static void LogException(Exception e)
+    {
+        Log(LogLevel.Warning, e.Message, e.StackTrace);
+    }
 
-	public static void Fatal(params object[] content)
-	{
-		MethodBase method = new StackTrace().GetFrame(1).GetMethod();
-		logger.LogFatal((object)("(" + method.ReflectedType.Name + "." + method.Name + "()) " + string.Join(" ", content)));
-	}
+    public static void LogFatal(params object[] content)
+    {
+        Log(LogLevel.Fatal, content);
+    }
 
-	public static void Warning(params object[] content)
-	{
-		MethodBase method = new StackTrace().GetFrame(1).GetMethod();
-		logger.LogWarning((object)("(" + method.ReflectedType.Name + "." + method.Name + "()) " + string.Join(" ", content)));
-	}
+    public static void LogWarning(params object[] content)
+    {
+        Log(LogLevel.Warning, content);
+    }
 
-	public static void Info(params object[] content)
-	{
-		MethodBase method = new StackTrace().GetFrame(1).GetMethod();
-		logger.LogInfo((object)("(" + method.ReflectedType.Name + "." + method.Name + "()) " + string.Join(" ", content)));
-	}
+    public static void LogInfo(params object[] content)
+    {
+        Log(LogLevel.Info, content);
+    }
 
-	public static void Debug(params object[] content)
-	{
-		MethodBase method = new StackTrace().GetFrame(1).GetMethod();
-		logger.LogDebug((object)("(" + method.ReflectedType.Name + "." + method.Name + "()) " + string.Join("  ", content)));
-	}
+    public static void LogDebug(params object[] content)
+    {
+        Log(LogLevel.Debug, content);
+    }
 
-	public static void Debugger(params object[] content)
-	{
-		Debug(content);
-	}
+    private static void Log(LogLevel level, params object[] content)
+    {
+        MethodBase method = new StackTrace().GetFrame(2).GetMethod();
+        logger.Log(level, $"({method.ReflectedType.Name}.{method.Name}()) {string.Join(" ", content)}");
+    }
 
-	public static string PrependTextToLog(string log, string text)
-	{
-		log = text + "\n" + log;
-		string[] array = log.Split('\n');
-		if (array.Length > DebuggerLines)
-		{
-			log = string.Join("\n", array, 0, DebuggerLines);
-		}
-		return log;
-	}
+    public static void Debugger(params object[] content)
+    {
+        LogDebug(content);
+    }
+
+    public static string PrependTextToLog(string log, string text)
+    {
+        log = text + "\n" + log;
+        string[] array = log.Split('\n');
+        if (array.Length > DebuggerLines)
+        {
+            log = string.Join("\n", array, 0, DebuggerLines);
+        }
+        return log;
+    }
 }
