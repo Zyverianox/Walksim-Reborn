cc9b1a1c6cfa469d14ce8f3df467a46745ec13bf
Vivid <130241472+Zyverianox@users.noreply.github.com>
2025-01-03 14:16:06 -0500

    Update HarmonyPatches.cs

--- WalkSimUpdater/WalkSim.Plugin/HarmonyPatches.cs
+++ WalkSimUpdater/WalkSim.Plugin/HarmonyPatches.cs
@@ -5,33 +5,31 @@ namespace WalkSim.Plugin;
 
 public class HarmonyPatches
 {
-	private static Harmony instance;
+    private static Harmony instance;
 
-	public const string InstanceId = "com.kylethescientist.gorillatag.walksimulator";
+    public const string InstanceId = "com.kylethescientist.gorillatag.walksimulator";
 
-	public static bool IsPatched { get; private set; }
+    public static bool IsPatched { get; private set; }
 
-	internal static void ApplyHarmonyPatches()
-	{
-		//IL_0024: Unknown result type (might be due to invalid IL or missing references)
-		//IL_002e: Expected O, but got Unknown
-		if (!IsPatched)
-		{
-			if (instance == null)
-			{
-				instance = new Harmony("com.kylethescientist.gorillatag.walksimulator");
-			}
-			instance.PatchAll(Assembly.GetExecutingAssembly());
-			IsPatched = true;
-		}
-	}
+    internal static void ApplyHarmonyPatches()
+    {
+        if (!IsPatched)
+        {
+            if (instance == null)
+            {
+                instance = new Harmony(InstanceId);
+            }
+            instance.PatchAll(Assembly.GetExecutingAssembly());
+            IsPatched = true;
+        }
+    }
 
-	internal static void RemoveHarmonyPatches()
-	{
-		if (instance != null && IsPatched)
-		{
-			instance.UnpatchSelf();
-			IsPatched = false;
-		}
-	}
+    internal static void RemoveHarmonyPatches()
+    {
+        if (instance != null && IsPatched)
+        {
+            instance.UnpatchSelf();
+            IsPatched = false;
+        }
+    }
 }
